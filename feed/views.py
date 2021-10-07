439c31887dc16f76a0b4624b681e211ad9029465
Daniel Arpide <djarpide@gmail.com>
2021-10-07 15:40:15 -0400

    Django lesson 517

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,8 @@
 from typing import List
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomePage(ListView):
 	http_method_names = ['get']
@@ -8,3 +10,25 @@ class HomePage(ListView):
 	model = Post
 	context_object_name = "posts"
 	queryset = Post.objects.all().order_by('-id')[0:30]
+
+class PostDetailView(DetailView):
+	http_method_name = ['get']
+	template_name = "feed/detail.html"
+	model = Post
+	context_object_name = "post"
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+	model = Post
+	template_name = "feed/create.html"
+	fields = ['text']
+	success_url = "/"
+
+	def dispatch(self, request, *args, **kwargs):
+		self.request = request
+		return super().dispatch(request, *args, **kwargs)
+
+	def form_valid(self, form):
+		obj = form.save(commit=False)
+		obj.author = self.request.user
+		obj.save()
+		return super().form_valid(form)
